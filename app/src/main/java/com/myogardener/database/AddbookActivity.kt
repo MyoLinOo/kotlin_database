1479f6e101358bd501abd855a5bafb86ae9556f0
myolinoo <myolinoo088555@gmail.com>
2020-11-04 12:46:43 +0630

    first

---
+++ app/src/main/java/com/myogardener/database/AddbookActivity.kt
@@ -0,0 +1,38 @@
+package com.myogardener.database
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.TextUtils
+import kotlinx.android.synthetic.main.activity_addbook.*
+
+
+class AddbookActivity : AppCompatActivity() {
+
+    lateinit var bookName:String
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addbook)
+
+        btn_save.setOnClickListener {
+            val replyIntent = Intent()
+            bookName = edt_bookname.text.toString()
+
+            if (TextUtils.isEmpty(edt_bookname.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            }else {
+
+                bookName = edt_bookname.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, bookName)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+    companion object{
+        const val EXTRA_REPLY="REPLY_BOOK"
+    }
+}
